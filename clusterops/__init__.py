81c97b12865260d5b15dec40b1136d3543b33ea3
Your Name <you@example.com>
2024-10-08 23:30:21 -0400

    [CLEANUP]

---
+++ clusterops/__init__.py
@@ -0,0 +1,9 @@
+from clusterops.main import (
+    list_available_cpus,
+    execute_with_cpu_cores,
+    list_available_gpus,
+    execute_on_gpu,
+    execute_on_multiple_gpus,
+)
+
+__all__ = ["list_available_cpus", "execute_with_cpu_cores", "list_available_gpus", "execute_on_gpu", "execute_on_multiple_gpus"]
